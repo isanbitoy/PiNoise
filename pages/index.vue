79b57fa5ad87a7627f156dc933ac091155d25251
isan <38833845+isanbitoy@users.noreply.github.com>
2018-09-03 22:16:14 +0800

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,33 @@
 <template>
-  <section class="section">
+  <section class="section-wrapper">
     <!--<div v-for="(articles, index) in processedArticles" v-bind:key="index">-->
-    <div class="cont" v-for="(article, index) in articles" v-bind:key="index">
-      <a v-bind:href="article.url" target="_blank">
-        <div class="card">
-          <div class="card-image">
-            <figure class="image">
-              <img v-bind:src="article.urlToImage" v-bind:alt="article.title" />
-            </figure>
-          </div>
+    <transition-group class="grid-content">
+      <div class="container" v-for="(article, index) in articles" v-bind:key="index">
+        <a v-bind:href="article.url" target="_blank">
+        <div class="card-image">
+          <figure class="image">
+            <img v-bind:src="article.urlToImage" v-bind:alt="article.title" width="300" height="200" />
+            <figcaption>{{ article.title }}</figcaption>
+          </figure>
         </div>
         <div class="card-content">
-          <div class="content">{{ article.title }}</div>
-          <div>{{ article.description }}</div>
+          <!--<div class="content">{{ article.description }}</div>-->
         </div>
-      </a>
-    </div>
+        </a>
+      </div>
+    </transition-group>
     <!--</div>-->
   </section>
 </template>
 
 <script>
 import axios from 'axios'
-/*const newsAPI = 'https://newsapi.org/v2/top-headlines?country=ph&apiKey=643d0a34867c44cc9519671ec2e0dfbd'*/
 
-const PHBaseUrl = 'https://newsapi.org/v2/top-headlines?country=ph'
+const BaseUrl = 'https://newsapi.org/v2/top-headlines?country=ph'
 const ApiKey = '643d0a34867c44cc9519671ec2e0dfbd'
 
 function buildUrl(url) {
-  return PHBaseUrl + "&category=" + url + "&apiKey=" + ApiKey
+  return BaseUrl + "&category=" + url + "&apiKey=" + ApiKey
 }
 export default {
   data() {
@@ -49,57 +48,32 @@ export default {
         .catch(error => {
           console.log(error)
         });
-    },
-
-  },
-  /*
-  computed: {
-    processedArticles() {
-      let posts = this.articles;
-
-      posts.map(post => {
-        let imgObj = posts.urlToImage;
-        post.image_url = imgObj ? imgObj.urlToImage : 'http://placehold.it/300x200?text=N/A';
-      });
-      let i, j, chunkedArray = [], chunk = 4;
-      for (i=0, j=0; i < posts.length; i += chunk, j++) {
-        chunkedArray[j] = posts.slice(i,i+chunk);
-      }
-      return chunkedArray;
     }
   }
-  */
 }
 </script>
 
 <style>
+.section-wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+.grid-content {
+    display: grid;
+    grid-gap: 6px;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 300px));
+    grid-auto-rows: 1fr;
+    grid-auto-flow: dense;
+}
 .container {
-  min-height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+    position: relative;
+    box-sizing: border-box;
+    border: 1px solid #d3d3d3;
 }
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.card-content {
+    position: absolute;
+    width: 300px;
 }
 </style>
 
