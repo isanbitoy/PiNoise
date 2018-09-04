e2b1f3d20beebb7ae49472f39016b5fa0cb27ea2
isan <38833845+isanbitoy@users.noreply.github.com>
2018-09-04 22:51:40 +0800

    Add files via upload

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,36 @@
 <template>
   <div>
-    <div>
-      <h1>Philippine News Portal</h1>
+    <div class="header-content">
+      <h1>Philippines News Portal</h1>
       <h3>All fresh headlines from Philippines</h3>
-      <div class="div-line"></div>
     </div>
+    <div class="div-line"></div>
     <nuxt/>
   </div>
 </template>
 
 <style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-  margin: 2.5em 12%;
+    font-family: Raleway, sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    margin: 2.5em 12%;
 }
 
 *, *:before, *:after {
-  box-sizing: border-box;
-  padding: 0;
-  margin: 0;
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+}
+h1, h3 {
+    color: #80ced6;
+}
+.header-content {
+    margin: 10px 0;
 }
 .div-line {
     position: relative;
