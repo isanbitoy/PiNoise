958bae0e92f129927f4c876057cb96c25652396d
isan <isanbitoy@gmail.com>
2018-11-06 23:54:38 +0800

    this is modified file

---
+++ components/TheNewsFooter.vue
@@ -0,0 +1,16 @@
+<template>
+	<footer>
+		<div class="footer-list">
+			<div class="copyright">
+				<span>&copy;</span>
+				<span>{{ year }}</span>
+				<span>All rights reserved</span>
+			</div>
+			<div class="attr-link">
+				| Powered by 
+				<a v-bind:href="newsApiUrl" target="_blank">
+				NewsAPI.org</a>
+			</div>
+		</div>
+	</footer>
+</template>
