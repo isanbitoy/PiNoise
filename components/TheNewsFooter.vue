9b17d7c7b784f535529b988b90cf672277574a0a
isan <isanbitoy@gmail.com>
2018-11-07 22:20:41 +0800

    init added files

--- components/TheNewsFooter.vue
+++ components/TheNewsFooter.vue
@@ -1,5 +1,13 @@
 <template>
 	<footer>
+		<ul class="nav-list">
+			<li><a v-on:click="setCategory('business')">Business</a></li>
+      		<li><a v-on:click="setCategory('entertainment')">Entertainment</a></li>
+      		<li><a v-on:click="setCategory('health')">Health</a></li>
+      		<li><a v-on:click="setCategory('science')">Science</a></li>
+      		<li><a v-on:click="setCategory('sports')">Sports</a></li>
+      		<li><a v-on:click="setCategory('technology')">Technology</a></li>
+		</ul>
 		<div class="footer-list">
 			<div class="copyright">
 				<span>&copy;</span>
@@ -8,9 +16,28 @@
 			</div>
 			<div class="attr-link">
 				| Powered by 
-				<a v-bind:href="newsApiUrl" target="_blank">
-				NewsAPI.org</a>
+				<a :href="newsApiUrl" 
+				    target="_blank">
+					NewsAPI.org
+				</a>
 			</div>
 		</div>
 	</footer>
 </template>
+<script>
+export default {
+	data: function() {
+		return {
+			year: new Date().getFullYear(),
+			newsApiUrl: 'https://newsapi.org/',
+      		vueUrl: 'https://vuejs.org/'
+		}
+	},
+	methods: {
+		setCategory: function() {
+			this.$store.dispatch('LOAD_ARTICLE_NEWS', category)
+      		this.$store.commit('SET_CATEGORY', category)
+		}
+	}
+};
+</script>
--- components/TheNewsHeader.vue
+++ components/TheNewsHeader.vue
@@ -1,9 +1,12 @@
 <template>
-  <header id="news-header">
+  <header>
     <section class="news-banner">
       <div class="flag-container"/>
       <div class="banner-name">
-        <div><span>Pi</span><span>Noise</span></div>
+        <div>
+          <span>Pi</span>
+          <span>Noise</span>
+        </div>
         <div>All Pinoy News</div>
       </div>
     </section>
@@ -24,77 +27,18 @@ export default {
         interval: ''
       }
     },
+     mounted: function() {
+      this.interval = setInterval(() => {
+        this.time()
+      }, 1000)
+    },
     methods: {
       time: function() {
         this.localeTime = moment().format('LTS').toLocaleString()
       }
-    },
-    mounted: function() {
-      this.interval = setInterval(() => {
-        this.time()
-      }, 1000)
     },
     beforeDestroy: function() {
       clearInterval(this.interval)
     }
 };
 </script>
-<style scoped>
-#news-header {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    width: 100%;
-    height: auto;
-    background: #222;
-    padding: 1em 0;
-}
-.news-banner {
-    flex: 1 1 auto;
-    display: inline-flex;
-    align-self: center;
-    justify-content: center;
-}
-.flag-container {
-    background-image: url('../assets/svg/ph.svg');
-    background-size: cover;
-    background-repeat: no-repeat;
-    box-sizing: content-box;
-    border: 1px solid #fff;
-    /*border-radius: 4px;*/
-    width: 3em;
-    padding: 8px;
-}
-.banner-name {
-    color: #fff;
-    margin: 0 6px;
-    cursor: pointer;
-}
-.banner-name div:last-child {
-    font-size: 17px;
-}
-.banner-name > div span:first-child {
-    font-size: 35px;
-    font-weight: bold;
-    letter-spacing: -1px;
-}
-.banner-name > div span:last-child {
-    font-size: 25px;
-    letter-spacing: -2px;
-}
-.news-date {
-    flex: 1 1 auto;
-    display: flex;
-    align-self: center;
-    justify-content: center;
-    flex-direction: column;
-    flex-wrap: wrap;
-    padding: 0 2.0em;
-    margin-top: 12px;
-    user-select: none;
-}
-.news-date span {
-    color: #fff;
-    text-align: center;
-}
-</style>
