6b6b02e8424f72f7613baeb61a8db16f2ea42625
isan <isanbitoy@gmail.com>
2018-11-09 21:10:06 +0800

    init added files

---
+++ components/TheNewsArticleNav.vue
@@ -0,0 +1,20 @@
+<template>
+	<nav>
+		<ul class="breadcrumb">
+			<li><a href="#business"><span @click="setCategory('business')">Business</span></a></li>
+			<li><a href="#health"><span @click="setCategory('health')">Health</span></a></li>
+      		<li><a href="#science"><span @click="setCategory('science')">Science</span></a></li>
+      		<li><a href="#sports"><span @click="setCategory('sports')">Sports</span></a></li>
+      		<li><a href="#technology"><span @click="setCategory('technology')">Technology</span></a></li>
+		</ul>
+	</nav>
+</template>
+<script>
+export default {
+	methods: {
+		setCategory: function(category) {
+			this.$store.dispatch('LOAD_ARTICLE_NEWS', category)
+		}
+	}
+};
+</script>
--- plugins/global-component.js
+++ plugins/global-component.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import NewsHeader from '@/components/TheNewsHeader'
 import NewsArticle from '@/components/TheNewsArticle'
-import NewsNav from '@/components/TheNewsNav'
+import NewsArticleNav from '@/components/TheNewsArticleNav'
 import NewsFooter from '@/components/TheNewsFooter'
 
 Vue.component('news-header', NewsHeader)
 Vue.component('news-article', NewsArticle)
-Vue.component('news-nav', NewsNav)
+Vue.component('news-article-nav', NewsArticleNav)
 Vue.component('news-footer', NewsFooter)
